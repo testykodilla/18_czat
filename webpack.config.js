772deb44a351e393690f0635d3926e25e6d873a4
marek <kursygitarowekopernik@gmail.com>
2018-02-25 15:23:31 +0100

    nie dziala. wyswietla mi w rpzegladarce bledy webpack require. Testowalem czy nie brakuje jakichs wtyczek do webpacka, przegladalem w necie polecane webpackowe konfiguracje itd i i tak nie dziala :( nie wiem jak to zrobic

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeJsPlugin = require('optimize-js-plugin');
+const path = require('path');
+var env = process.env.NODE_ENV || 'development';
+var plugins = [
+    new HtmlWebpackPlugin({
+        template: 'client/index.html',
+        filename: 'index.html',
+        inject: 'body',
+    })
+];
+
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+plugins.push(
+    new webpack.optimize.UglifyJsPlugin(),
+    new OptimizeJsPlugin({
+        sourceMap: false
+    })
+  );
+}
+
+module.exports = {
+    entry: (env !== 'production' ? [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:8081',
+        'webpack/hot/only-dev-server',
+    ] : []).concat(['./client/index.js']),
+    output: {
+        filename: './bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    entry: [
+        'react-hot-loader/patch',
+        './client/index.js'
+    ],
+    entry: './client/index.js',
+        output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: plugins
+};
